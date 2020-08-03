d26b56aefbc262efcf8dbd9884e6cec603eff4ac
unknown <aroldo@gmail.com>
2020-08-02 21:36:50 -0300

    Corrigindo o problema da navbar.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,22 +14,22 @@ const isUsuarioAutenticado = () => {
 */
 function Navbar(props){
     return (
-        <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
             <div className="container">
                 <a href="#/home" className="navbar-brand">Minhas Finanças</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">                    
                     <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav">
-                    <NavbarItem render={props.isUsuarioAutenticado} href="#/home" label="Home"/>
-                    <NavbarItem render={props.isUsuarioAutenticado} href="#/cadastroUsuarios" label="Usuários"/>
-                    <NavbarItem render={props.isUsuarioAutenticado} href="#/consultaLancamentos" label="Lançamentos"/>
-                    <NavbarItem render={props.isUsuarioAutenticado} onClick={props.deslogar} href="#/login" label="Sair"/>                    
-                </ul>
+                </button>                
+                <div className="navbar-collapse collapse" id="navbarResponsive">
+                    <ul className="navbar-nav mr-auto">
+                        <NavbarItem render={props.isUsuarioAutenticado} href="#/home" label="Home"/>
+                        <NavbarItem render={props.isUsuarioAutenticado} href="#/cadastroUsuarios" label="Usuários"/>
+                        <NavbarItem render={props.isUsuarioAutenticado} href="#/consultaLancamentos" label="Lançamentos"/>
+                        <NavbarItem render={props.isUsuarioAutenticado} onClick={props.deslogar} href="#/login" label="Sair"/>                    
+                    </ul>
+                </div>
             </div>
-        </div>
-    </div>
+        </nav>
     )
 }
 
